acf3e6ff2ba13273e4e6512b3f471e192d5c92a7
Patrick Kim <pkim050@gmail.com>
2019-05-16 18:56:48 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,17 @@
 class SessionsController < ApplicationController
-  before_action :current_user
-
   def new
 
   end
 
   def create
-
+    if params[:name].empty? || params[:name].nil?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+    end
   end
 
   def destroy
     session.clear unless session[:name].empty? || session[:name].nil?
-  end
-
-  private
-
-  def current_user
-    return head(:forbidden) unless session.include? :user_id
   end
 end
