74d43fb1bc6602d0f63b8034cc7b85e4e32bb741
Patrick Kim <pkim050@gmail.com>
2019-05-16 18:54:36 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,3 +1,13 @@
 class SecretsController < ApplicationController
+  before_action :require_login
+  
+  def show
+    redirect_to controller: 'sessions', action: 'new' if 
+  end
 
+  private
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
 end
