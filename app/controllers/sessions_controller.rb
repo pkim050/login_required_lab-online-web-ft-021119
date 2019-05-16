19bde708a44f988db549cc6447acab03abfe3577
Patrick Kim <pkim050@gmail.com>
2019-05-16 19:55:57 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if current_user.nil?
+    if params[:name].nil? || params[:name].empty?
       redirect_to '/login'
     else
       session[:name] = params[:name]
